47f03d4d8b74faa9400699907d1059c59d98c966
rrusss <rrussarybayev@gmail.com>
2020-11-21 13:33:44 +0600

    requirements updated

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import User
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'date_joined', )
     list_filter = ('date_joined',)
     search_fields = ('username',)
